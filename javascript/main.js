d1ab285fc679347e963a5f88984693dca9c403bf
carlossroliveira <63623377+carlossroliveira@users.noreply.github.com>
2020-12-19 21:44:54 -0300

    Style

--- javascript/main.js
+++ javascript/main.js
@@ -1,2 +1,24 @@
+function selecaoDoInput(evento) {
+
+    evento.preventDefault();
+
+    const elementUl = document.querySelector('[data-list]');
+    const elementInput = document.querySelector('[data-form-input]');
+    const valor = elementInput.value;
+
+    const elementLi = document.createElement('li');
+          elementLi.classList.add('elementActive');
+
+    const elementP = `<p>${valor}</p>`;
+
+    elementLi.innerHTML = elementP;
+    elementUl.appendChild(elementLi);
+         
+    elementInput.value = '';
+}
+
+const elementBtn = document.querySelector('[data-form-btn]');
+
+elementBtn.addEventListener('click', selecaoDoInput);
 
 
